5c3ce1a0d30e2994716d344b34d01e2b50b4a19b
Fredrik Forssen <fredde@grodslok.net>
2015-09-08 21:33:18 +0200

    Serialize silly simple class

---
+++ SillyXml.Tests/XmlSerializerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace SillyXml.Tests
+{
+
+    
+
+    [TestFixture]
+    public class XmlSerializerTests
+    {
+        public class SimpleClass
+        {
+            public int Foo { get; } = 42;
+            public string Bar { get; } = "Banana";
+        }
+
+        [Test]
+        public void Serialize_Simple_Class()
+        {
+            var str = XmlSerializer.Serialize(new SimpleClass());
+            Assert.IsNotNull(str);
+        }
+
+    }
+}
---
+++ SillyXml/XmlSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace SillyXml
+{
+    public class XmlSerializer
+    {
+        public static string Serialize(object obj)
+        {
+            var root = ToXml(obj);
+            return root.ToString();
+        }
+
+        private static XElement ToXml(object obj)
+        {
+            var type = obj.GetType();
+            var el = new XElement(type.Name);
+
+            foreach(var property in type.GetRuntimeProperties())
+            {
+                var value = property.GetMethod.Invoke(obj, new object[0]);
+                el.Add(new XElement(property.Name, value));
+            }
+            return el;
+        }
+
+    }
+}
