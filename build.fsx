a8c2585b162d27e632edcee5c686a043cc190a6a
Fredrik Forssen <fredde@grodslok.net>
2015-09-11 08:58:18 +0200

    Kicked version

--- build.fsx
+++ build.fsx
@@ -8,7 +8,7 @@ let packagingDir = "./packaging/"
 
 let appReferences = [ "SillyXml/SillyXml.csproj" ]
 
-let version = "0.1.5"
+let version = "0.1.6"
 
 Target "Clean" (fun _ -> 
     CleanDirs [ buildDir; packagingDir; ]
